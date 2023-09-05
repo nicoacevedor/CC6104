0d2291f72da1995d33cdc65153ff6b0f7afc9642
nicoacevedor <nico.acevedo.rivas@gmail.com>
2023-09-05 02:02:04 -0300

    P5 modificada

--- Tarea 1/ruina.R
+++ Tarea 1/ruina.R
@@ -16,17 +16,26 @@ ruina <- function(money = 100, bet = 5) {
 
 n_bet <- 5000
 bet_val <- 50
-bet_vector <- vector(length = n_bet)
+bet5_vector <- vector(length = n_bet)
+bet20_vector <- vector(length = n_bet)
+bet50_vector <- vector(length = n_bet)
 for (i in 1:n_bet) {
-  final_money <- tail(ruina(bet = bet_val), 1)
-  if (final_money <= 0) {
-    win <- 0
-  } else if (final_money >= 200) {
-    win <- 1
-  } else {
-    print("Error: Funcion 'ruina' no llega a 0 o 200")
-    break
-  }
-  bet_vector[i] <- win
+  final5_money <- tail(ruina(bet = 5), 1)
+  final20_money <- tail(ruina(bet = 20), 1)
+  final50_money <- tail(ruina(bet = 50), 1)
+  win5 <- if (final5_money <= 0) 0 else 1
+  win20 <- if (final20_money <= 0) 0 else 1
+  win50 <- if (final50_money <= 0) 0 else 1
+  bet5_vector[i] <- win5
+  bet20_vector[i] <- win20
+  bet50_vector[i] <- win50
 }
-hist(bet_vector, main = sprintf(""))
+t5 <- table(bet5_vector)
+t20 <- table(bet20_vector)
+t50 <- table(bet50_vector)
+print(sprintf("Apuesta = 5 -> E = %d, F = %d, Prob = %f", t5["1"], t5["0"], t5["1"]/(t5["1"] + t5["0"])))
+hist(bet5_vector, main = sprintf("Éxitos y fracasos para apuesta = 5"))
+print(sprintf("Apuesta = 20 -> E = %d, F = %d, Prob = %f", t20["1"], t20["0"], t20["1"]/(t20["1"] + t20["0"])))
+hist(bet20_vector, main = sprintf("Éxitos y fracasos para apuesta = 20"))
+print(sprintf("Apuesta = 50 -> E = %d, F = %d, Prob = %f", t50["1"], t50["0"], t50["1"]/(t50["1"] + t50["0"])))
+hist(bet50_vector, main = sprintf("Éxitos y fracasos para apuesta = 50"))
