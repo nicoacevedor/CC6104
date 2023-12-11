b898c470b29aff828fcb3d6afb2ba360cfb38283
nicoacevedor <nico.acevedo.rivas@gmail.com>
2023-12-11 14:10:18 -0300

    P4 lista

--- Tarea 5/mcmc.R
+++ Tarea 5/mcmc.R
@@ -1,6 +1,6 @@
-gamma_func <- function(x, alpha, beta) {
+gamma_func <- function(x, shape, rate) {
   if (x > 0)
-    return(x^(alpha - 1) * exp(-beta * x))
+    return(x^(shape - 1) * exp(-rate * x))
   else
     return(0)
 }
@@ -12,8 +12,8 @@ metropolis <- function(theta_0, n_iter, alpha, beta) {
   for (k in 1:n_iter + 1) {
     theta[k] <- current
     proposal <- rnorm(1, mean = theta[k], sd = 1)
-    prob_prop <- pgamma(proposal, shape = alpha, rate = beta)
-    prob_current <- pgamma(current, shape = alpha, rate = beta)
+    prob_prop <- gamma_func(proposal, shape = alpha, rate = beta)
+    prob_current <- gamma_func(current, shape = alpha, rate = beta)
     ratio <- prob_prop / prob_current
     prob_decision <- min(1, ratio)
     decision <- rbinom(1, 1, prob_decision)
@@ -23,6 +23,6 @@ metropolis <- function(theta_0, n_iter, alpha, beta) {
   return(theta[-1])
 }
 
-outcomes <- metropolis(1, 5e5, 5, 0.2)
+outcomes <- metropolis(1, 1e5, 5, 0.2)
 
 hist(outcomes)
